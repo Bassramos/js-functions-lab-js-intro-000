c3a8331294d568d7840f9ec4f63ded9c4f5c9fe2
Joshua Ramos <bassramos@gmail.com>
2020-09-16 02:46:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function happyHolidays(string) {
 function happyHolidaysTo(name) {
   return(`Happy holidays, ${name}!`)
 }
-function happyCustomHolidayTo(string) {
+function happyCustomHolidayTo(holiday) {
   return(`Happy ${holiday} ${name}!`)
 }
