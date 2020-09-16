5b7716cc57ef7fde73cef93575d4b1ea054c7c4a
Joshua Ramos <bassramos@gmail.com>
2020-09-16 01:56:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays(string) {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(name) {
+  console.log(`Happy holidays, ${name}!`)
+}
+function happyCustomHolidayTo(string) {
+  console.log(`Happy ${holiday}, ${name}!`)
+}
